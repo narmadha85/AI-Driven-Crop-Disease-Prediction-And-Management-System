8073648470f314bb28944b6aed852ed717a50211
narmadha85 <narmadhaarun7@gmail.com>
2026-06-17 14:16:14 +0530

    Add files via upload

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,33 @@
-console.log("Dashboard Loaded");
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", function () {
+
+  let filter = searchInput.value.toLowerCase();
+
+  let rows = document.querySelectorAll("#tableBody tr");
+
+  rows.forEach(function(row) {
+
+    let cropName = row.cells[1].textContent.toLowerCase();
+
+    if (cropName.includes(filter)) {
+
+      row.style.display = "";
+
+    } else {
+
+      row.style.display = "none";
+
+    }
+
+  });
+
+});
+
+function deleteRow(button) {
+
+  let row = button.parentElement.parentElement;
+
+  row.remove();
+
+}
