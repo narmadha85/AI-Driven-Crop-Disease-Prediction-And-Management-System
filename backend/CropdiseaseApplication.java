cfd29a0676fc7845b8062151bf991b971b8f9dfa
narmadha85 <narmadhaarun7@gmail.com>
2026-07-07 14:12:10 +0530

    Add files via upload

---
+++ backend/CropdiseaseApplication.java
@@ -0,0 +1,12 @@
+package com.crop.cropdisease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropdiseaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropdiseaseApplication.class, args);
+    }
+}
