3b34b901465e80f3ee16d9089f13f13dff78b30c
narmadha85 <narmadhaarun7@gmail.com>
2026-07-08 07:00:52 +0530

    Add files via upload

---
+++ backend/CropapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cropapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
